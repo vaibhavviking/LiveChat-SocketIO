e6752eab011c64b269873866668f547efebd2ad7
vaibhav <chandravaibhav65@gmail.com>
2021-02-08 00:25:19 +0530

    made app

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const app = express();
+
+const server = app.listen(8000, () => {
+    console.log('listening to request on port 8000');
+});
+
+app.use(express.static('public'))
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"]
+    }
+});
+
+const users = {};
+
+io.on('connection', socket => {
+    // If any new user joins, let other users connected to the server know!
+    socket.on('new-user-joined', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    // If someone sends a message, broadcast it to other people
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
+    });
+
+    // If someone leaves the chat, let others know 
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+})
